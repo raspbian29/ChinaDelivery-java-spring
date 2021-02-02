6a24bb2a9869739408ec92795e38951341dcfa58
Victor <talmaci.victor@gmail.com>
2021-02-02 19:43:44 +0200

    fix

--- src/main/java/com/chinabox/delivery/controllers/FileUploadController.java
+++ src/main/java/com/chinabox/delivery/controllers/FileUploadController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 
 @CrossOrigin(origins = "http://localhost:4200")
