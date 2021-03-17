2b923bb7b8b1bec42f62ef4055cf58827abe51b7
Victor <talmaci.victor@gmail.com>
2021-03-17 18:29:50 +0200

    minor image upload fixes

--- src/main/java/com/chinabox/delivery/controllers/FileUploadController.java
+++ src/main/java/com/chinabox/delivery/controllers/FileUploadController.java
@@ -3,6 +3,7 @@ package com.chinabox.delivery.controllers;
 import com.chinabox.delivery.dao.PackageRequestRepository;
 import com.chinabox.delivery.model.PackagePhoto;
 import com.chinabox.delivery.model.PackageRequest;
+import com.chinabox.delivery.model.User;
 import com.chinabox.delivery.model.UserType;
 import com.chinabox.delivery.service.PackagePhotoService;
 import com.chinabox.delivery.service.RestControllerService;
@@ -11,13 +12,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
-
 @RestController
 @RequestMapping("rest/upload")
 public class FileUploadController {
@@ -45,17 +45,16 @@ public class FileUploadController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
-   @PostMapping(value = "uploadBase64", consumes = {"multipart/form-data"})
-    public ResponseEntity<Void> uploadPhoto2(@RequestParam("imageFile") String[] base64images,
-                                            @RequestParam("packageRequestId") Long id) {
+    @PostMapping(value = "uploadBase64", consumes = {"multipart/form-data"})
+    public ResponseEntity<Void> uploadPhoto2(@RequestParam("images") String[] base64images,
+                                             @RequestParam("packageRequestId") Long id) {
         Optional<PackageRequest> packageRequest = this.packageRequestRepository.findById(id);
         UserType userRole = restControllerService.requestUser().getRole();
         boolean isUserAllowed = userRole == UserType.ADMINISTRATOR || userRole == UserType.OPERATOR;
         if (!(packageRequest.isPresent() && isUserAllowed)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-        for (String base64img: base64images) {
-
+        for (String base64img : base64images) {
             packagePhotoService.uploadBase64(base64img, packageRequest.get());
         }
         return ResponseEntity.status(HttpStatus.OK).build();
@@ -63,15 +62,20 @@ public class FileUploadController {
 
 
     @GetMapping(value = "getPackagePhotos")
-    public List<PackagePhoto> getImage(@RequestParam("packageRequestId") Long id) {
-
-        List<PackagePhoto> packagePhotos = new ArrayList<>();
-        UserType userRole = restControllerService.requestUser().getRole();
-        boolean isUserAllowed = userRole == UserType.ADMINISTRATOR || userRole == UserType.OPERATOR;
-        if (isUserAllowed) {
-            return packagePhotoService.getImages(id);
+    public ResponseEntity<List<PackagePhoto>> getImage(@RequestParam("packageRequestId") Long id) {
+        Optional<PackageRequest> pro = packageRequestRepository.findById(id);
+        if (pro.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        return packagePhotos;
+        PackageRequest pr = pro.get();
+        User requestUser = restControllerService.requestUser();
+        User prUser = pr.getUser();
+        UserType userRole = requestUser.getRole();
+        boolean isUserAllowed = userRole == UserType.ADMINISTRATOR || userRole == UserType.OPERATOR;
+        if (!prUser.getId().equals(requestUser.getId()) || !isUserAllowed) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(packagePhotoService.getImages(id));
     }
 
 }
